26dc4ecdcba2f0ce372a2b6565f8426c9b39490b
Prof.: Edvaldo Rodrigues <86244745+profedvaldo@users.noreply.github.com>
2023-04-06 14:33:49 -0300

    Update tabela_professores.sql

--- tabela_professores.sql
+++ tabela_professores.sql
@@ -1,4 +1,5 @@
-CREATE TABLE professor ( id INTEGER AUTO_INCREMENT PRIMARY KEY, nome VARCHAR(100), email VARCHAR(255), cidade VARCHAR(100), id_materia INTEGER);
+CREATE TABLE professor ( id INTEGER AUTO_INCREMENT PRIMARY KEY, nome VARCHAR(100), 
+                        email VARCHAR(255), cidade VARCHAR(100), id_materia INTEGER);
 
 CREATE TABLE materia ( id INTEGER AUTO_INCREMENT PRIMARY KEY, descricao VARCHAR(100));
 
@@ -17,3 +18,17 @@ INSERT INTO materia (descricao) VALUES ("Mobile");
 INSERT INTO materia (descricao) VALUES ("Análise de Sistemas");
 INSERT INTO materia (descricao) VALUES ("Ciências da Computação");
 INSERT INTO materia (descricao) VALUES ("Banco de Dados***");
+
+SELECT * from materia;
+
+INSERT INTO professor (nome, email, materia) VALUES ("Ferdinanda", "ferdinanda@escola.pr.gov.br", 1);
+INSERT INTO professor (nome, email, materia) VALUES ("Conrrado", "conrrad@escola.pr.gov.br", 5);
+INSERT INTO professor (nome, email, materia) VALUES ("Edvaldo", "edvaldo.rodrigues@escola.pr.gov.br", 15);
+INSERT INTO professor (nome, email, materia) VALUES ("Edvaldo", "edvaldo.rodrigues@escola.pr.gov.br", 13);
+
+INSERT INTO professor (nome, email, id_materia) VALUES ("Ferdinanda", "ferdinanda@escola.pr.gov.br", 1);
+
+INSERT INTO professor (nome, email, id_materia) VALUES ("Conrrado", "conrrad@escola.pr.gov.br", 5);
+
+INSERT INTO professor (nome, email, id_materia) VALUES ("Edvaldo", "edvaldo.rodrigues@escola.pr.gov.br", 15);
+INSERT INTO professor (nome, email, id_materia) VALUES ("Edvaldo", "edvaldo.rodrigues@escola.pr.gov.br", 13);
