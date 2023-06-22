173818fb9dd2e4f0437450086bd3a4f3de238740
Prof.: Edvaldo Rodrigues <86244745+profedvaldo@users.noreply.github.com>
2023-06-22 17:00:26 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,19 +7,25 @@ $password ="";
 $dbname ="empregados";
 
 $conexao = mysqli_connect($hostname, $username, $password, $dbname) or die("Não foi possível fazer a conexão ao banco de dados.");
-
+?>
+<form method="GET">
+    <label>Nome</label>
+    <input type="text" name="txt_nome" />
+    <input type="submit" value="salvar" /><br />
+</form>
+<?php
 //mysqli_select_db($dbnome)
 
 /*vou fazer uma pesquisa no banco de dados
 empregados e mostrar os dados*/
 
-$sql = "SELECT nome, id FROM empregado";
+$sql = "SELECT nome, id FROM empregado ORDER BY id DESC LIMIT 0,6";
 
 $resultado = mysqli_query($conexao, $sql);
 
 if($resultado){
     while($linha = mysqli_fetch_array($resultado)){
-        echo $linha["id"]." - ".$linha["nome"];
+        echo $linha["id"]." - ".$linha["nome"]."<br />";
     }
 }
 ?>
