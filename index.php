4e480e6f60ee36c665ec893ae639775f1405e902
Prof.: Edvaldo Rodrigues <86244745+profedvaldo@users.noreply.github.com>
2023-06-22 13:41:27 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,28 @@
 <?php
-//aqui vamos conectar a base de dados
-//config da base
-$host = "10.209.67.224";
-$user = "root";
-$pass = "";
-$banc = "empregados";
+//aqui vou comecar o banco de dados
 
-mysqli_connect($host,$user,$pass,$banc) or die ("não conectou a banco de dados"); 
+$hostname = "10.209.67.224";
+$username = "root";
+$password = "";
+$dbname = "empregados";
+
+$conexao = mysqli_connect($hostname, $username, $password, $dbname) or die ("Não foi possível conectar ao banco de dados"); 
+
+//mysqli_select_db($dbname);
+
+//aqui vou fazer uma pesquisa no banco de dados empregados e mostrar os
+
+$sql = "SELECT nome, id * FROM empregado";
+
+$resultado = mysqli_query($conexao, $sql);
+
+if($resultado){
+    while($linha = mysqli_fech_array($resultado)){
+        echo $linha["id"]." - ".$linha["nome"];
+    }
+}
+
+
+
+  
+?>
